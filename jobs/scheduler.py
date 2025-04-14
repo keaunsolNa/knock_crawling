dbf0a0c79a071890a82522be3d7139d9e8a0cd21
나큰솔 <knsol1992@e-hcg.com>
2025-04-14 10:56:45 +0900

    롯데 크롤링

--- jobs/scheduler.py
+++ jobs/scheduler.py
@@ -1,19 +1,21 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
-from datetime import date
 import asyncio
-from crawling.services.kopis import KOPISCrawler
-from crawling.services.kofic import KOFICCrawler
 import os
 from dotenv import load_dotenv
+from datetime import date
+from crawling.services import CGVCrawler, MEGABOXCrawler, LOTTECrawler, KOFICCrawler, KOPISCrawler
+from infra.es_utils import save_to_es
+
 
 load_dotenv()
 
-async def run_kopis():
+async def run_scheduler():
 
     one_year_ago_january_first = date.today().replace(year=date.today().year - 1, month=1, day=1)
+    start_date = date.today().replace(year=date.today().year - 1).strftime("%Y")
 
-    config = {
+    kopis_config = {
         "url": os.getenv("KOPIS_API_URL"),
         "params": {
             "service": os.getenv("KOPIS_API_KEY"),
@@ -23,28 +25,82 @@ async def run_kopis():
             "rows": "100"
         }
     }
-    crawler = KOPISCrawler(config)
-    results = crawler.crawl()
-    print(f"[SCHEDULER] KOPIS crawled {len(results)} items")
-
-async def run_kofic():
-    start_date = date.today().replace(year=date.today().year - 1).strftime("%Y")
-    config = {
+    kofic_config = {
         "url": os.getenv("KOFIC_API_URL"),
+        "url_sub": os.getenv("KOFIC_API_URL_SUB"),
         "params": {
             "key": os.getenv("KOFIC_API_KEY"),
             "openStartDt": start_date,
-            "itemPerPage": "10"
+            "itemPerPage": "100"
         }
     }
-    crawler = KOFICCrawler(config)
-    results = crawler.crawl()
-    print(f"[SCHEDULER] KOFIC crawled {len(results)} items")
+
+    megabox_config = {
+        "url": os.getenv("MEGABOX_API_URL"),
+        "url_sub": os.getenv("MEGABOX_API_URL_SUB")
+    }
+
+    cgv_config = {
+        "url": os.getenv("CGV_API_URL"),
+        "url_sub": os.getenv("CGV_API_URL_SUB")
+    }
+
+    lotte_config = {
+        "url": os.getenv("LOTTE_API_URL"),
+        "url_sub": os.getenv("LOTTE_API_URL_SUB")
+    }
+
+    try:
+        kopis = KOPISCrawler(kopis_config)
+        result = kopis.crawl()
+        print("📦 KOPIS 결과 총 수량", len(result))
+        save_to_es(os.getenv("BONSAI_INDEX_KOPIS"), result, dedup_keys=["name", "start_date"])
+    except Exception as e:
+        print("❌ KOPIS 실패:", e)
+
+    try:
+        kofic = KOFICCrawler(kofic_config)
+        result = kofic.crawl()
+        print("📦 KOFIC 결과 총 수량", len(result))
+        save_to_es(os.getenv("BONSAI_INDEX_KOFIC"), result, dedup_keys=["movieNm", "openDt"])
+    except Exception as e:
+        print("❌ KOFIC 실패:", e)
+
+    try:
+        megabox = MEGABOXCrawler(megabox_config)
+        result = megabox.crawl()
+        print("📦 MEGABOX 결과 총 수량", len(result))
+        print(result)
+
+        save_to_es(os.getenv("BONSAI_INDEX_MOVIE"), result, dedup_keys=["movieNm", "openDt"])
+    except Exception as e:
+        print("❌ MEGABOX 실패:", e)
+
+    try:
+        cgv = CGVCrawler(cgv_config)
+        result = cgv.crawl()
+        print("📦 CGV 결과 총 수량", len(result))
+        print(result)
+
+        save_to_es(os.getenv("BONSAI_INDEX_MOVIE"), result, dedup_keys=["movieNm", "openDt"])
+    except Exception as e:
+        print("❌ CGV 실패:", e)
+
+    try:
+        lotte = LOTTECrawler(lotte_config)
+        result = lotte.crawl()
+        print("📦 LOTTE 결과 총 수량", len(result))
+        print(result)
+
+        save_to_es(os.getenv("BONSAI_INDEX_MOVIE"), result, dedup_keys=["movieNm", "openDt"])
+    except Exception as e:
+        print("❌ LOTTE 실패:", e)
+
 
 async def main():
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(run_kopis, CronTrigger(second="*/30"))
-    scheduler.add_job(run_kofic, CronTrigger(second="*/30"))
+    trigger = CronTrigger(hour=22, minute=0, timezone="Asia/Seoul")
+    scheduler.add_job(run_scheduler, trigger)
     scheduler.start()
     await asyncio.Event().wait()
 
